c89a5d0dd6aae60c94d1d05e4c88e66a53fc88ef
cherishh <wzx1991@gmail.com>
2025-08-28 08:25:51 +0800

    feat: update

--- module-1/studio/router.py
+++ module-1/studio/router.py
@@ -2,8 +2,13 @@ from langchain_openai import ChatOpenAI
 from langgraph.graph import MessagesState
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Tool
+
+
 def multiply(a: int, b: int) -> int:
     """Multiplies a and b.
 
@@ -13,13 +18,19 @@ def multiply(a: int, b: int) -> int:
     """
     return a * b
 
+
 # LLM with bound tool
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    model="gpt-4o")
 llm_with_tools = llm.bind_tools([multiply])
 
 # Node
+
+
 def tool_calling_llm(state: MessagesState):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
 
 # Build graph
 builder = StateGraph(MessagesState)
